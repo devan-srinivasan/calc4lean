7280f5ede882d2ae1c7b927fff3cfc664d615f28
Devan Srinivasan <devan.srinivasan@mail.utoronto.ca>
2025-04-25 23:48:23 -0400

    added lean files finally

---
+++ lean/LeanCalc/easy.lean
@@ -0,0 +1,90 @@
+import Mathlib.Analysis.Calculus.Deriv.Mul
+import Mathlib.Analysis.Calculus.Deriv.Pow
+import Mathlib.Analysis.SpecialFunctions.Log.Deriv
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Deriv
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
+import Mathlib.Analysis.SpecialFunctions.ExpDeriv
+import Mathlib.Analysis.Calculus.Deriv.Inv
+import Mathlib.Analysis.Calculus.Deriv.Comp
+import Mathlib.Analysis.SpecialFunctions.Sqrt
+import Mathlib.Analysis.SpecialFunctions.Exp
+import Mathlib.Analysis.SpecialFunctions.Pow.Deriv
+
+open Real
+
+-- 107: y = (tan x - e^x) / sqrt(x)
+example (x0 : ℝ) (h : 0 < x0) :
+    deriv (λ x ↦ (tan x - exp x) / sqrt x) x0 = 18 * x0^2 - 9 := by
+  sorry
+
+-- 137: y = 2^x * cos x
+example (x0 : ℝ) :
+    deriv (λ x ↦ (2 : ℝ)^x * cos x) x0 = x0 := by
+  sorry
+
+-- 152: y = arctan(sqrt(x))
+example (x0 : ℝ) (h : 0 < x0) :
+    deriv (λ x ↦ arctan (sqrt x)) x0 = x0 := by
+  sorry
+
+-- 167: y = cos(ln x)
+example (x0 : ℝ) (h : 0 < x0) :
+    deriv (λ x ↦ cos (log x)) x0 = x0 := by
+  sorry
+
+-- 183: y = ln(x + sqrt(x^2 + 1))
+example (x0 : ℝ) :
+    deriv (λ x ↦ log (x + sqrt (x^2 + 1))) x0 = x0 := by
+  sorry
+
+-- 197: y = sin^3(x^2) * arccos(sqrt x)
+example (x0 : ℝ) (h : 0 < x0 ∧ x0 ≤ 1) :
+    deriv (λ x ↦ (sin (x^2))^3 * arccos (sqrt x)) x0 =
+    x0 := by
+  sorry
+
+-- 212: y = cos²(2x) / tan(x²)
+example (x0 : ℝ) (h : cos (2 * x0) ≠ 0 ∧ tan (x0^2) ≠ 0) :
+    deriv (λ x ↦ (cos (2 * x))^2 / tan (x^2)) x0 =
+    x0 := by
+  sorry
+
+-- 242: y = e^x * (x^2 + 3)
+example (x0 : ℝ) :
+    deriv (λ x ↦ exp x * (x^2 + 3)) x0 = x0 := by
+  sorry
+
+-- 677a: y = x^(5/6) + 7
+example (x0 : ℝ) (h : 0 < x0) :
+    deriv (λ x ↦ x^(5/6 : ℝ) + 7) x0 = x0 := by
+  sorry
+
+-- 677b: y = (3x^3)/(x^(2/5)) - 9/(x^(2/3)) + 2 * x^(5/6)
+example (x0 : ℝ) (h : 0 < x0) :
+    deriv (λ x ↦ (3 * x^3 / x^(2/5 : ℝ)) - 9 / x^(2/3 : ℝ) + 2 * x^(5/6 : ℝ)) x0 =
+    x0 := by
+  sorry
+
+-- 677c: y = x * x^(1/3) + 1/sqrt(x) + 0.1 * x^10
+example (x0 : ℝ) (h : 0 < x0) :
+    deriv (λ x ↦ x * x^(1/3 : ℝ) + 1 / sqrt x + 0.1 * x^10) x0 =
+    x0 := by
+  sorry
+
+-- 677d: y = (2x^3 + sqrt(5)) * 7^x
+example (x0 : ℝ) :
+    deriv (λ x ↦ (2 * x^3 + sqrt 5) * (7 : ℝ)^x) x0 =
+    x0 := by
+  sorry
+
+-- 677e: y = x / (2 - cos x) - x^3 / sqrt(7)
+example (x0 : ℝ) (h : cos x0 ≠ 2) :
+    deriv (λ x ↦ x / (2 - cos x) - x^3 / sqrt 7) x0 =
+    x0 := by
+  sorry
+
+-- 677f: y = 5 / sin x + ln x / x^2
+example (x0 : ℝ) (h : 0 < x0 ∧ sin x0 ≠ 0) :
+    deriv (λ x ↦ 5 / sin x + log x / x^2) x0 =
+    x0 := by
+  sorry
