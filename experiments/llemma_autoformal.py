8489b7eb80606f53b629dbe15a0df2abaf0b9344
MeganDeer <mg908939@dal.ca>
2025-05-13 22:15:17 +0000

    small updates

--- experiments/llemma_autoformal.py
+++ experiments/llemma_autoformal.py
@@ -1,19 +1,21 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+from exp import ProblemSolver
 
 # their paper says they use prompts in the format of draft sketch, and prove to formalize
 # draft, sketch, and prove uses few-shot prompting for their autoformlaizer
 # TODO: get examples for prompts
 
-class Llemma_Autoformalizer():
-    def __init__(self):
+class Llemma_Autoformalizer(ProblemSolver):
+    def __init__(self, name: str = ""):
+        self.name = name
         model_name = "EleutherAI/llemma_7b"
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModelForCausalLM.from_pretrained(model_name)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = self.model.to(self.device)
 
-    def formalize(self, prompt, max_new_tokens: int = 64):
+    def solve_hint(self, prompt, max_new_tokens: int = 64):
         inputs = self.tokenizer(prompt, return_tensors='pt').to(self.device)
         output_ids = self.model.generate(
             **inputs,
@@ -27,3 +29,5 @@ class Llemma_Autoformalizer():
             clean_up_tokenization_spaces=True,
         )
         
+    def solve_nohint():
+        return
