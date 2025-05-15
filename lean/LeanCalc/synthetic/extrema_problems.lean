32c5c92c410dfbe56160c283085b5c30983fd7a5
Samarendra Dash <samarendra109@gmail.com>
2025-05-14 20:51:57 -0400

    tangent and extrema

---
+++ lean/LeanCalc/synthetic/extrema_problems.lean
@@ -0,0 +1,69 @@
+
+import Mathlib.Analysis.Calculus.Deriv.Add
+import Mathlib.Analysis.Calculus.Deriv.Mul
+import Mathlib.Analysis.Calculus.Deriv.Pow
+import Mathlib.Analysis.Calculus.Deriv.Comp
+
+open Real
+
+example (f:ℝ→ℝ) : (f = fun x:ℝ =>  x ^ 3 - 7 * x ^ 2 + 16 * x) → (deriv f (2:ℝ) = 0 ∧ deriv (deriv f) (2:ℝ) < 0) := by
+  intros hf
+  have h_deriv_f : deriv f = fun x =>  3 * x ^ 2 - 14 * x + 16 := by
+    ext x
+    rw [hf]
+    nth_rewrite 1 [deriv_add]
+    nth_rewrite 1 [deriv_sub]
+    nth_rewrite 1 [deriv_pow'']
+    nth_rewrite 1 [deriv_id'']
+    nth_rewrite 1 [deriv_mul]
+    nth_rewrite 1 [deriv_const]
+    nth_rewrite 1 [deriv_pow'']
+    nth_rewrite 1 [deriv_id'']
+    nth_rewrite 1 [deriv_mul]
+    nth_rewrite 1 [deriv_const]
+    nth_rewrite 1 [deriv_id'']
+    
+    ring
+    exact differentiableAt_const _
+    exact differentiableAt_id
+    exact differentiableAt_id
+    exact differentiableAt_const _
+    exact differentiableAt_pow _
+    exact differentiableAt_id
+    exact differentiableAt_pow _
+    exact DifferentiableAt.mul (differentiableAt_const _) (differentiableAt_pow _)
+    exact DifferentiableAt.sub (differentiableAt_pow _) (DifferentiableAt.mul (differentiableAt_const _) (differentiableAt_pow _))
+    exact DifferentiableAt.mul (differentiableAt_const _) (differentiableAt_id)
+
+  have h_deriv_deriv_f : deriv (deriv f) = fun x =>  6 * x - 14 := by
+    ext x
+    rw [h_deriv_f]
+    nth_rewrite 1 [deriv_add]
+    nth_rewrite 1 [deriv_sub]
+    nth_rewrite 1 [deriv_mul]
+    nth_rewrite 1 [deriv_const]
+    nth_rewrite 1 [deriv_pow'']
+    nth_rewrite 1 [deriv_id'']
+    nth_rewrite 1 [deriv_mul]
+    nth_rewrite 1 [deriv_const]
+    nth_rewrite 1 [deriv_id'']
+    nth_rewrite 1 [deriv_const]
+    
+    ring
+    exact differentiableAt_const _
+    exact differentiableAt_id
+    exact differentiableAt_id
+    exact differentiableAt_const _
+    exact differentiableAt_pow _
+    exact DifferentiableAt.mul (differentiableAt_const _) (differentiableAt_pow _)
+    exact DifferentiableAt.mul (differentiableAt_const _) (differentiableAt_id)
+    exact DifferentiableAt.sub (DifferentiableAt.mul (differentiableAt_const _) (differentiableAt_pow _)) (DifferentiableAt.mul (differentiableAt_const _) (differentiableAt_id))
+    exact differentiableAt_const _
+
+  constructor
+  nth_rewrite 1 [h_deriv_f]
+  ring
+  nth_rewrite 1 [h_deriv_deriv_f]
+  ring
+  norm_num
+    
