38eab80d875f3ae63138032e2099d3721b19e460
MeganDeer <mg908939@dal.ca>
2025-05-16 17:56:01 +0000

    deepseek pt1

--- experiments/run_exp.py
+++ experiments/run_exp.py
@@ -3,5 +3,5 @@ from llemma_autoformal import Llemma_Autoformalizer
 from theoremllama_autoformal import TL_Autoformalizer
 from exp import run_exp_nohint
 
-solver = Llemma_Autoformalizer(name="llemma")
+solver = DS_Autoformalizer(name="deepseek")
 run_exp_nohint(problem_file="lean/LeanCalc/generated_data/inequality_10.lean",solver=solver)
