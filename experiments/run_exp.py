aa21bf80a621125375a32ae1e47df4bf5b68776a
Samarendra Dash <samarendra109@gmail.com>
2025-05-18 11:30:58 -0400

    O4-results

--- experiments/run_exp.py
+++ experiments/run_exp.py
@@ -1,13 +1,13 @@
 from deepseek_autoformal import DS_Autoformalizer
 from llemma_autoformal import Llemma_Autoformalizer
 from theoremllama_autoformal import TL_Autoformalizer
-from r1_autoformal import DeepSeekR1ProblemSolver
+from openai_autoformal import OpenAIReasoningProblemSolver
 from exp import run_exp_nohint
 
-path = 'lean/LeanCalc/generated_data/'
-#files = ['inequality.lean','extrema_problems.lean','differentiation_632.lean','monotone_A_100.lean','monotone_B_100.lean','pq_easy.lean','tangents.lean']
-files = ['monotone_A_100.lean','monotone_B_100.lean']
-solver = DeepSeekR1ProblemSolver(name="r1")
-for file in files:
-    print(file)
-    run_exp_nohint(problem_file=path+file,solver=solver)
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='run_exp_monotonicity.log', level=logging.INFO)
+
+solver = OpenAIReasoningProblemSolver("o4-mini")
+logger.info(f"Logging for solver {solver.__class__}")
+run_exp_nohint(problem_file="lean/LeanCalc/generated_data/monotonicity.lean",solver=solver, force_new_results=True)
