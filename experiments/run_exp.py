73f8a0f3b4aed8b24523541724df0e9a84cd9f63
MeganDeer <mg908939@dal.ca>
2025-05-18 01:49:15 +0000

    trying to get git to cooperate

--- experiments/run_exp.py
+++ experiments/run_exp.py
@@ -1,7 +1,13 @@
 from deepseek_autoformal import DS_Autoformalizer
 from llemma_autoformal import Llemma_Autoformalizer
 from theoremllama_autoformal import TL_Autoformalizer
+from r1_autoformal import DeepSeekR1ProblemSolver
 from exp import run_exp_nohint
 
-solver = DS_Autoformalizer(name="deepseek")
-run_exp_nohint(problem_file="lean/LeanCalc/generated_data/inequality_10.lean",solver=solver)
+path = 'lean/LeanCalc/generated_data/'
+#files = ['inequality.lean','extrema_problems.lean','differentiation_632.lean','monotone_A_100.lean','monotone_B_100.lean','pq_easy.lean','tangents.lean']
+files = ['monotone_A_100.lean','monotone_B_100.lean']
+solver = DeepSeekR1ProblemSolver(name="r1")
+for file in files:
+    print(file)
+    run_exp_nohint(problem_file=path+file,solver=solver)
